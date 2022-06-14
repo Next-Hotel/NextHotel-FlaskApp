30b24e4ff423243f4ce16511615d0e9f4707b418
Kelvin Ding <70010690+kelvin-ding@users.noreply.github.com>
2022-06-14 22:35:36 +0700

    changes code

--- data_hotel.py
+++ data_hotel.py
@@ -1,12 +1,10 @@
-import encodings
-from numpy import unicode_
 import pandas as pd 
 
 def load_data(data):
-	df = pd.read_csv(data)
+	df = pd.read_csv(data,encoding='unicode_escape')
 	return df 
 
 def get_list_hotel():
-    df = load_data("list-hotel/Data API.csv")
+    df = load_data("https://storage.googleapis.com/data-hotel/list-hotel/list-hotels.csv")
     results_json = df.to_json(orient ='table')
     return(results_json)
