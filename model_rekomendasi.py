5734142294f615003d46dee334728e6d815d9a42
Christell James Cellose <73575878+ChristellJamesCellose@users.noreply.github.com>
2022-06-14 15:08:01 +0700

    Add files via upload
    Data preprocess v1

--- model_rekomendasi.py
+++ model_rekomendasi.py
@@ -1,3 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+
+# Data Preprocessing
+df = pd.read_csv('https://storage.googleapis.com/data-hotel/list-hotel/data_preprocess.csv')
+
+df = df.drop(df.columns[[0, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]], axis=1)
+print(df.head())
+
+print(df.columns)
+
+X = df[['star','reviews','harga', 'Shuttle Service', 'Sports and Recreations', 'Kids and Pets', 'Transportation', 'Connectivity', 'Accessibilty', 'Things to Do', 'General', 'Public Facilities', 'Nearby Facilities', 'Business Facilities', 'In-room Facilities', 'Hotel Services', 'Food and Drinks', 'Fast Food', 'Shop & Gifts', 'Business', 'Transportation Hub', 'Casual Dining', 'Nightlife', 'Park & Zoo', 'Public Service', 'Arts & Sciences', 'Fine Dining', 'Sport', 'Quick Bites', 'Education', 'Street Food', 'Activity & Games', 'Cafe', 'Entertainment', 'Food Court', 'Sight & Landmark' ]]
+y = df['rating']
+
+# Data Splitting
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+dev_X, val_X, dev_y, val_y = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+train_dataset = df.loc[:70]
+test_dataset = df.drop(train_dataset.index)
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('rating')
+test_labels = test_features.pop('rating')
+
+# Weighted Ranking (JSON)
+
+# Modelling
+
 # Coba Ngoding Model disini
 
 def model_rekomendasi(data):
